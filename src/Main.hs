1f43151543be60533b99381c5beddd91da44b611
cobb <cobb@afb2f898-e5c3-4517-9fc9-9156763f6afe>
2009-12-20 11:15:22 +0000

    Split Hemkay core and released new version.
    git-svn-id: https://sgate.emt.bme.hu/svn/haskell/hemkay@255 afb2f898-e5c3-4517-9fc9-9156763f6afe

--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,23 @@
-import Sound.PortAudio.MB
+import Control.Monad
+import Control.Monad.Fix
+import Sound.Hemkay.Loader
+import Sound.Hemkay.Mixer
+import Sound.PortAudio hiding (chunk)
 import System.Environment
-
-import Loader
-import Player
 
 main :: IO ()
 main = do
-  initialize
   args <- getArgs
   song <- loadModule (head args)
-  playModule song
-  terminate
+  withPortAudio $
+    withDefaultStream 0 2 paFloat32 (realToFrac sampleFrequency) 0x1000 $ \stream _ -> do
+      flip fix (mixSong song) $ \playback chunks -> unless (null chunks) $ do
+        let (state,chunk):songRest = chunks
+        flip fix chunk $ \push chk -> unless (null chk) $ do
+          Right len <- getStreamWriteAvailable stream
+          let (pre,rest) = splitAt len chk
+          writeStream stream (map (\(Smp sl sr) -> [sl,sr]) pre) len
+          push rest
+        putStr (show state)
+        playback songRest
+  return ()
